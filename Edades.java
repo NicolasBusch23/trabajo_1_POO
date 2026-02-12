0c8bd62c913af556bffb53fd78406b681f8903bd
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 01:34:18 -0500

    Se crea un primer método estático dentro de la clase Edades que permite calcular la edad de Alberto

---
+++ Edades.java
@@ -0,0 +1,7 @@
+public class Edades {
+    //Método o función para calcular la edad de Alberto
+    public static double calcular_edad_alberto(double edad_juan) {
+        double edad_alberto = 2 * edad_juan / 3;
+        return edad_alberto;
+    }
+}
