250742116c4bf922724d0c5c413b75a07f8949de
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 02:01:04 -0500

    Se crea la clase que resuelve el ejercicio 4 usando los metodos definidos en la clase 'Edades'

---
+++ Resolución_Ejercicio_4.java
@@ -0,0 +1,17 @@
+public class Resolución_Ejercicio_4 {
+        public static void main(String[] args) {
+
+        double edad_juan = 9;
+        double edad_alberto, edad_ana, edad_mama;
+
+        //Se usan los 3 métodos definidos en la clase 'Edades'
+        edad_alberto = Edades.calcular_edad_alberto(edad_juan);
+        edad_ana = Edades.calcular_edad_ana(edad_juan);
+        edad_mama = Edades.calcular_edad_mama(edad_juan);
+
+        System.out.println("Las edades son: Alberto = " + edad_alberto + 
+        " años, Juan = " + edad_juan + " años, Ana = " + edad_ana + 
+        " años, Mamá = "+ edad_mama +" años.");
+    }
+}
+
