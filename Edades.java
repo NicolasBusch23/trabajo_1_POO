b8d93bf37420febdfa18777bf5b7aeb4dedec5c4
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 01:37:28 -0500

    Se crea un segundo y tercer método estático dentro de la clase Edades que permite calcular la edad de Ana y de la Mamá

--- Edades.java
+++ Edades.java
@@ -4,4 +4,16 @@ public class Edades {
         double edad_alberto = 2 * edad_juan / 3;
         return edad_alberto;
     }
+
+    //Método o función para calcular la edad de Ana
+    public static double calcular_edad_ana(double edad_juan) {
+        double edad_ana = 4 * edad_juan / 3;
+        return edad_ana;
+    }
+
+    //Método o función para calcular la edad de Mamá
+    public static double calcular_edad_mama(double edad_juan) {
+        double edad_mama = edad_juan + calcular_edad_alberto(edad_juan) + calcular_edad_ana(edad_juan);
+        return edad_mama;
+    }
 }
