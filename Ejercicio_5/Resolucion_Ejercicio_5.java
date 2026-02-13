15c0323663b91734fff9e74b81a845daa1772a75
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 23:14:24 -0500

    Se modifica el programa principal utilizando los métodos definidos en la clase 'Operaciones_Intermedias'

--- Ejercicio_5/Resolucion_Ejercicio_5.java
+++ Ejercicio_5/Resolucion_Ejercicio_5.java
@@ -1,14 +1,16 @@
 package Ejercicio_5;
-
+//Programa Principal para resolver el ejercicio 5
 public class Resolucion_Ejercicio_5 {
     public static void main(String[] args) {
         double suma, x, y;
         suma = 0;
         x = 20;
-        suma = suma + x;
+        suma = Operaciones_Intermedias.primer_actualizacion(suma, x);
+
         y = 40;
         x = x + Math.pow(y,2);
-        suma = suma + x/y;
+        suma = Operaciones_Intermedias.segunda_actualizacion(suma, x, y);
+
         System.out.println("El valor de la suma es: " + suma);
     }
 }
