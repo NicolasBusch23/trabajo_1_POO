dff7fa9610930b2905337b136399e37b46c9cd46
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 21:56:10 -0500

    Se implementan los métodos definidos en la clase 'Operaciones' dentro del programa principal

--- Ejercicio_12/Resolución_Ejercicio_12.java
+++ Ejercicio_12/Resolución_Ejercicio_12.java
@@ -6,7 +6,14 @@ public class Resolución_Ejercicio_12 {
         double valor_hora = 5000;
         double porcentaje_retencion_fuente = 0.125;
 
-        double salario_bruto, retencion_fuente, salario_neto;
+        //Se usan los 3 métodos definidos en la clase 'Operaciones'
+        double salario_bruto = Operaciones.calcular_salario_bruto(horas_trabajadas, valor_hora);
+        double retencion_fuente = Operaciones.calcular_retencion_fuente(salario_bruto, porcentaje_retencion_fuente);
+        double salario_neto = Operaciones.calcular_salario_neto(salario_bruto, retencion_fuente);
+
+        System.out.println("El salario bruto del trabajador es: $" + salario_bruto);
+        System.out.println("La retención en la fuente del salario del trabajador es: $" + retencion_fuente);
+        System.out.println("El salario neto del trabajador es: $" + salario_neto);
         
     }
 }
