a00d3d6971bf6e317d4864d9004b98b30c0680ed
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 23:12:29 -0500

    Se crean los métodos que realizan las operaciones intermedias que actualizan la variable 'suma'. Esto, en la clase 'Operaciones_intermedias'

--- Ejercicio_5/Operaciones_Intermedias.java
+++ Ejercicio_5/Operaciones_Intermedias.java
@@ -1,5 +1,10 @@
 package Ejercicio_5;
-
 public class Operaciones_Intermedias {
+    public static double primer_actualizacion(double suma, double x) {
+        return suma + x;
+    }
 
+    public static double segunda_actualizacion(double suma, double x, double y) {
+        return suma + x/y;
+    }
 }
