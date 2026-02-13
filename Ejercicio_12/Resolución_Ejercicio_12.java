e5df89404e91d87ab379ce600c9da7030f5f4264
Nicolás <nicolasbusch5@gmail.com>
2026-02-12 21:38:17 -0500

    Se inicializan variables

---
+++ Ejercicio_12/Resolución_Ejercicio_12.java
@@ -0,0 +1,12 @@
+package Ejercicio_12;
+//Programa Principal para resolver el ejercicio 12
+public class Resolución_Ejercicio_12 {
+    public static void main(String[] args) {
+        double horas_trabajadas = 48;
+        double valor_hora = 5000;
+        double porcentaje_retencion_fuente = 0.125;
+
+        double salario_bruto, retencion_fuente, salario_neto;
+        
+    }
+}
